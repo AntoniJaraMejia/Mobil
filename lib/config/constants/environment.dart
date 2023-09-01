56d9f39b66c85c6c724f656b7dcaaf7d8c6a4c4f
Antoni Jara <antoni.jara@gmail.com>
2023-09-01 11:24:22 -0500

    cinepedia introduccion

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Enviorement {
-  static String movieDbKey = dotenv.env['THE_MOVIEDB_KEY'] ?? 'No hay api key';
+  static String themovieDbKey = dotenv.env['THE_MOVIEDB_KEY'] ?? 'No hay api key';
 }
